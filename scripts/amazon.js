c5d1cbddf5b11f64d1307b9c5124a60fbb6482bf
Nathan Hoang <nhoang6@lsu.edu>
2026-07-15 14:14:48 -0500

    Copy of L 14 Module

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-
+import {formatCurrency} from './utils/money.js';
 
 let productsHTML = '';
 
@@ -25,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
+        $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
@@ -60,18 +60,16 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function updateCartQuantity() 
-{
+function updateCartQuantity() {
   let cartQuantity = 0;
 
-      cart.forEach((cartItem) => {
-        cartQuantity += cartItem.quantity;
-      });
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 }
-
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
