49a522cb439af941a152c1590869716c9f03b82c
Nathan Hoang <nhoang6@lsu.edu>
2026-07-17 15:15:44 -0500

    automated tests for formatCurrency

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,10 +1,16 @@
 import {formatCurrency} from '../scripts/utils/money.js';
 
+console.log('test suite: formatCurrency');
+
+console.log('converts cents into dollars');
+
 if (formatCurrency(2095) === '20.95') {
   console.log('Test passed');
 } else {
   console.log('Test failed');
 }
+
+console.log('works with 0');
 
 if (formatCurrency(0) === '0.00') 
   {
@@ -12,6 +18,8 @@ if (formatCurrency(0) === '0.00')
   } else {
     console.log('Test failed');
   }
+
+console.log('rounds to the nearest cent');
 
 if (formatCurrency(2000.5) === '20.01') 
   {
