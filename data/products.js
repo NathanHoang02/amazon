430451c10e7fa5a57568cd647e1da901edff6827
Nathan Hoang <nhoang6@lsu.edu>
2026-08-10 15:03:35 -0500

    Details about classes and this

--- data/products.js
+++ data/products.js
@@ -62,6 +62,41 @@ class Clothing extends Product
     </a>`; 
   }
 }
+/*
+const date = new Date(); 
+console.log(date); 
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this)
+const object2 = 
+{
+  a: 1, 
+  b: this.a
+}
+*/
+
+/*
+function logThis()
+{
+  console.log(this);
+}
+
+logThis();
+logThis.call('hello');
+
+
+this
+const object3 = 
+{
+  method: () => 
+    {
+      console.log(this);
+    }
+};
+object3.method();
+*/
 
 export const products = [
   {
