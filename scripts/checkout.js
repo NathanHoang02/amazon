fb53d659e6f1d8bbc767557117d70b260db3bd91
Nathan Hoang <nhoang6@lsu.edu>
2026-08-14 14:46:28 -0500

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,12 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import {loadProducts} from './checkout/loadProducts.js';
 //import '../data/cart-class.js';
-
 //import '../data/car.js';
+// import '../data/backend-practice.js';
 
-import '../data/backend-practice.js';
-
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => 
+{
+  renderOrderSummary();
+  renderPaymentSummary();
+});
